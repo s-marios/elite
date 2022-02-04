d28ab920c0e9c92847a7d1044649e763bf931f61
marios <haha@myzen.jaist.ac.jp>
2022-02-04 10:37:25 +0900

    Refactor createDataProperty2 tests

--- tests/tests.c
+++ tests/tests.c
@@ -8,46 +8,84 @@
 #include "../elite_priv.h"
 #include "../elite.h"
 
-static void null_test_success(void **state) {
-	(void) state;
-}
-
-static void test_data_property2(void **state) {
+/***** testing dataProperty2 *****/
+static void test_data_property2_basic_setup_is_ok(void **state)
+{
 	Property_PTR prop = createDataProperty2(
-	                        0x80U, E_READ | E_WRITE, STORAGE_UPTO, 10, 4, "test");
+	        0x80U, E_READ | E_WRITE, STORAGE_UPTO, 10, 4, "test");
 	assert_non_null(prop);
 	assert_ptr_equal(prop->readf, readData);
 	assert_ptr_equal(prop->writef, writeData);
 	assert_int_equal(prop->rwn_mode, E_READ | E_WRITE);
 	assert_non_null(prop->opt);
+}
 
+static int setup_dataProperty2(void ** state)
+{
+	Property_PTR prop = createDataProperty2(
+	        0x80U, E_READ | E_WRITE, STORAGE_UPTO, 10, 4, "test");
+	*state = prop;
+	return 0;
+}
+
+static void test_dataProperty2_state_usage(void **state)
+{
+	assert_non_null(*state);
+	Property_PTR prop = *state;
+	assert_int_equal(prop->propcode, 0x80U);
+}
+
+static void test_initial_read_data_property2(void ** state)
+{
+	Property_PTR prop = createDataProperty2(
+	        0x80U, E_READ | E_WRITE, STORAGE_UPTO, 10, 4, "test");
 	//initial read is test
 	unsigned char read_buff[10];
 	int res = readProperty(prop, 10, read_buff);
 	assert_int_equal(res, 4);
 	int memcmp_res = memcmp(read_buff, "test", res);
 	assert_int_equal(memcmp_res, 0);
+}
 
+static void test_write_10_bytes_is_ok(void ** state)
+{
+	Property_PTR prop = createDataProperty2(
+	        0x80U, E_READ | E_WRITE, STORAGE_UPTO, 10, 4, "test");
 	const unsigned char * write_data = (const unsigned char *) "0123456789toolong";
+	unsigned char read_buff[10];
 	//write test
-	res = writeProperty(prop, 10, write_data);
+	int res = writeProperty(prop, 10, write_data);
 	assert_int_equal(res, 10);
 	res = readProperty(prop, 10, read_buff);
 	assert_int_equal(res, 10);
-	memcmp_res = memcmp(read_buff, write_data, res);
+	int memcmp_res = memcmp(read_buff, write_data, res);
 	assert_int_equal(memcmp_res, 0);
+}
 
-	//write something too long
-	res = writeProperty(prop, 15, write_data);
-	//failed max size check returns 0
+static void test_write_11_bytes_fails( void ** state)
+{
+	Property_PTR prop = createDataProperty2(
+	        0x80U, E_READ | E_WRITE, STORAGE_UPTO, 10, 4, "test");
+	const unsigned char * write_data = (const unsigned char *) "0123456789toolong";
+	//the property only stores 10 bytes, so a write of 11 should fail
+	int res = writeProperty(prop, 11, write_data);
 	assert_int_equal(res, 0);
 }
 
-int main(void) {
-	const struct CMUnitTest tests[] = {
-		cmocka_unit_test(null_test_success),
-		cmocka_unit_test(test_data_property2),
+
+int main(void)
+{
+	const struct CMUnitTest dataProperty2_tests[] = {
+		cmocka_unit_test_setup(test_dataProperty2_state_usage, setup_dataProperty2),
+		cmocka_unit_test(test_data_property2_basic_setup_is_ok),
+		cmocka_unit_test(test_initial_read_data_property2),
+		cmocka_unit_test(test_write_10_bytes_is_ok),
+		cmocka_unit_test(test_write_11_bytes_fails),
 	};
 
-	return cmocka_run_group_tests(tests, NULL, NULL);
+	char * group_name = "dataProperty2";
+	printf("\nGroup Test: %s\n", group_name);
+	cmocka_run_group_tests_name(group_name, dataProperty2_tests, NULL, NULL);
+
+	return 0;
 }
