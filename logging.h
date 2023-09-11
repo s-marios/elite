adf9023d198ab393dadb5175bd94b04c9dd680dd
marios <haha@myzen.jaist.ac.jp>
2023-09-11 15:43:20 +0900

    Simplify logging, remove unused weblog macro

--- logging.h
+++ logging.h
@@ -3,16 +3,7 @@
 
 #ifdef __unix
 //just to get us working on linux
-
 #include <stdio.h>
-#ifndef PRINTF
-#define PRINTF printf
-
-#endif
-
-#ifndef PPRINTF
-#define PPRINTF printf
-#endif
 
 #elif ESP_PLATFORM
 #include "freertos/FreeRTOS.h"
@@ -27,58 +18,26 @@
 #endif
 
 /**
- * Debug printfs. Debug level of 0 (zero) suppresses all output. Debug level of
- * 1 enables PRINTF, 2 enables PPRINTF also.
+ * Debug printfs, default level 1. Debug level of 0 (zero) suppresses all output.
+ * Debug level of 1 enables PRINTF, level 2 also enables PPRINTF.
  */
-//#define ELITE_DEBUG 2
-//#define PPRINTF printf
+
+#ifndef ELITE_DEBUG
+#define ELITE_DEBUG 1
+#endif
+
+#if ELITE_DEBUG == 0
+#define PRINTF(...)
 #define PPRINTF(...)
-/**
- * if defined, web log functionality at port 6666 is enabled.
- */
-//#define DOWEBLOG
 
-#ifdef DOWEBLOG
+#endif
 
-extern uint8_t ndsize;  /**< last weblog data size */
-extern char ndbuf[256]; /**< buffer holding the data to be sent */
-/** semaphore protecting simultaneous writes */
-extern SemaphoreHandle_t debugdowrite;
-/** semaphore used to signal  the performance of a write */
-extern SemaphoreHandle_t debugsem;
-
-/** the web logging macro, usually mapped to PPRINTF */
-#define WEBLOG(...)                                      \
-    do {                                                 \
-        if (debugsem) {                                  \
-            xSemaphoreTake(debugdowrite, portMAX_DELAY); \
-            ndsize = sprintf(ndbuf, __VA_ARGS__);        \
-            xSemaphoreGive(debugsem);                    \
-            xSemaphoreGive(debugdowrite);                \
-        }                                                \
-    } while (0)
-
-#else
-#define WEBLOG
-#endif //DOWEBLOG
-
-#ifdef ELITE_DEBUG
-#undef PRINTF
-#ifdef DOWEBLOG
-#define PRINTF WEBLOG
-#else
+#if ELITE_DEBUG == 1
 #define PRINTF printf
-#endif //DOWEBLOG
-#endif //ELITE_DEBUG
+#define PPRINTF(...)
+#endif
+
 #if ELITE_DEBUG > 1
-#undef PPRINTF
-#ifdef DOWEBLOG
-#define PPRINTF WEBLOG
-#else
-#define PPRINTF printf
-#endif
-#endif
-
-#ifndef PRINTF
 #define PRINTF printf
+#define PPRINTF printf
 #endif
